e022eebea24a2c8f9fd839e00e5624ef6cc346d0
Vaidik Dave <vaidikdave@Vaidiks-MacBook-Air.local>
2025-05-03 11:00:58 +0530

    Added DAY2 work

---
+++ DAY2/Calculator.java
@@ -0,0 +1,33 @@
+package DAY2;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your first number here");
+        int num1=sc.nextInt();
+        System.out.println("enter operator here");
+        char operator=sc.next().charAt(0);
+        System.out.println("enter secound num here");
+        int num2=sc.nextInt();
+
+        switch(operator){
+            case '+':
+            System.out.println(num1+num2);
+            break;
+            case'-':
+            System.out.println(num1-num2);
+            case'*':
+            System.out.println(num1*num2);
+            break;
+            case'/':
+            System.out.println(num1/num2);
+            break;
+            default:
+            System.out.println("not valid sign");
+        }
+    }
+    
+}
---
+++ DAY2/hello.java
@@ -0,0 +1,22 @@
+package DAY2;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt();
+        
+        System.out.println(a%2==0?"even":"odd");
+
+    }
+
+  
+    
+
+
+
+
+
+    
+}
