d6efd267c4a9ac2c5d27c45aea0947fcb7cf98a7
Vaidik Dave <vaidikdave@Vaidiks-MacBook-Air.local>
2025-05-07 08:45:17 +0530

    Added  DAY5 folder

---
+++ DAY4/naya.java
@@ -0,0 +1,11 @@
+package DAY4;
+
+import java.lang.reflect.Array;
+
+public class naya {
+    public static void main(String[] args) {
+        int array[]={1,2,4,5};
+        int[] a=new int[10];
+    }
+    
+}
---
+++ DAY5/wrapper.java
@@ -0,0 +1,22 @@
+package DAY5;
+/*
+ * wrapper class allows us to overcome the 
+ * primitive datatype limitations and allows
+ * us to have special methods and convert the primitive data types into object and
+ * also we can use it for the collection framework
+ */
+public class wrapper {
+
+    public static void main(String[] args) {
+        System.out.println(Byte.MIN_VALUE+"to"+Byte.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE+"to"+Short.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE+"to"+Integer.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE+"to"+Long.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE+"to"+Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE+"to"+Double.MAX_VALUE);
+      //  System.out.println(Boolean.MIN_VALUE+"to"+Boolean.MAX_VALUE);   
+      System.out.println(Character.MIN_VALUE+"to"+Character.MAX_VALUE);
+      
+    }
+    
+}
